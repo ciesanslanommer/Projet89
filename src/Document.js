d8fa136c619680402b2b0e0d1d50c3f9f9e0252d
evodable <vodableemma@gmail.com>
2021-01-12 15:32:47 +0100

    [Fix] Images path

--- src/Document.js
+++ src/Document.js
@@ -6,8 +6,8 @@ function Document({path, desc, format}){
     return(
         <div className="souvenir">
             <h1>{desc}</h1>
-            {format === "jpg" ? 
-                <img src={'../souvenirs/' + path + '.jpg'} alt={desc}></img> 
+            {format === 'jpg' || format === 'png' ? 
+                <img src={require('./../public/souvenirs/' + path + "." + format).default} alt={desc}></img> 
                 : <p>{raw(`../public/souvenirs/${path}.txt`).substr(0,500).concat("...")}</p>
             }
         </div>
