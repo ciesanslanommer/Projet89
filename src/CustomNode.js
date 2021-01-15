503da47c060f0101372d99bffff9e6ee0ff71425
Erwann <erwannlefevre8@gmail.com>
2021-01-15 11:43:55 +0100

    Visited memory

--- src/CustomNode.js
+++ src/CustomNode.js
@@ -1,11 +1,10 @@
-//import {Node} from 'react-d3-graph'
-
 function CustomNode({node}){
     let highlighted = node.highlighted
     let format = node.format
+    let visited = node.visited
+    console.log(node.visited)
     function Format(format, highlighted) {
-        let id = 1 
-        highlighted ? id = 2 : id = 1
+        let id = highlighted ? 2 : 1
         switch (format) {
             case 'jpg':
                 return <img src={require('./assets/photo' + id + '.png').default} alt= {node.name}/>
@@ -15,23 +14,18 @@ function CustomNode({node}){
                 return <img src={require('./assets/texte' + id + '.png').default} alt= {node.name}/>
             }
     }
-    return(
-        <div>
-            {Format(format, highlighted)}
-        </div>
-    );
+    if(visited){
+        return(
+            <div style = {{backgroundColor : 'red', width : 100, height: 100, borderRadius : 50}}></div>
+        )
+    }
+    else{
+        return(
+            <div>
+                {Format(format, highlighted)}
+            </div>
+        );
+    }
 }
 
 export default CustomNode
-
-
-/* { format == "jpg" ?
-highlighted ? 
-   <img src={require('./assets/photo2.png').default} alt= {node.name}/>
-   : <img src={require('./assets/photo1.png').default} alt= {node.name}/>
-:
-highlighted ? 
-   <img src={require('./assets/texte2.png').default} alt= {node.name}/>
-   : <img src={require('./assets/texte1.png').default} alt= {node.name}/>
-
-}*/
