fb0e4695874be2b23b0f88095578545a1f156d18
evodable <vodableemma@gmail.com>
2021-01-18 15:18:11 +0100

    [fix] fix warning

--- src/Document.js
+++ src/Document.js
@@ -40,7 +40,7 @@ function Audio(props) {
 }
 
 function Video(props) {
-    var path = props.parcours ? props.parcours + "/" : ""
+    //var path = props.parcours ? props.parcours + "/" : ""
     return (
         /*<video controls>
             <source src={require('./souvenirs/'+ path + props.path).default}
@@ -48,7 +48,7 @@ function Video(props) {
             </source>
             Sorry, your browser doesn't support embedded videos.
         </video>*/
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/jXZAbnn1kTU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe title={props.key} width="560" height="315" src="https://www.youtube.com/embed/jXZAbnn1kTU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     );
 }
 
