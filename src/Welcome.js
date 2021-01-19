02456412feba581ba745fa8272ac0690c999e815
Natachaliao32 <natachaliao32@gmail.com>
2021-01-19 13:59:41 +0100

    darken background + close when clicking elsewhere

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,20 +2,24 @@ import './Welcome.css';
 
 const Welcome = (props) => {
     return (
-        <div id='welcome'>
-            <h2>Bienvenue !</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat, metus in facilisis iaculis, magna orci eleifend augue, tincidunt sollicitudin quam quam quis arcu. Etiam facilisis neque id congue blandit. Phasellus in posuere arcu, ac porta nisl. Morbi vel sapien eros. Suspendisse potenti. Nam scelerisque eu tellus ac fermentum. In hac habitasse platea dictumst. Maecenas nisl dui, rhoncus sed nisl eget, malesuada accumsan erat. Maecenas vitae mauris id quam ullamcorper vulputate. Donec euismod ut ligula nec gravida.
-            </p>
-            <p>Pour naviguer dans la carte, utilisez le drag et le zoom.</p>
-            <img
-                id='cross'
-                src={require('./assets/close.png').default}
-                alt='cross'
-                onClick={props.onCrossClick}
-            >
-            </img>
+        <div id="welcome_wrap">
+            <div id='welcome'>
+                <h2>Bienvenue !</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat, metus in facilisis iaculis, magna orci eleifend augue, tincidunt sollicitudin quam quam quis arcu. Etiam facilisis neque id congue blandit. Phasellus in posuere arcu, ac porta nisl. Morbi vel sapien eros. Suspendisse potenti. Nam scelerisque eu tellus ac fermentum. In hac habitasse platea dictumst. Maecenas nisl dui, rhoncus sed nisl eget, malesuada accumsan erat. Maecenas vitae mauris id quam ullamcorper vulputate. Donec euismod ut ligula nec gravida.
+                </p>
+                <p>Pour naviguer dans la carte, utilisez le drag et le zoom.</p>
+                <img
+                    id='cross'
+                    src={require('./assets/close.png').default}
+                    alt='cross'
+                    onClick={props.onCrossClick}
+                >
+                </img>
+            </div>
+            <div class='darken_background' onClick={props.onCrossClick}></div>
         </div>
+
     );
 
 };
