19a612c1cdda42cacda5dd3fd320e0b039f70b79
Erwann <erwannlefevre8@gmail.com>
2021-01-19 16:12:48 +0100

    update css

--- src/CustomNode.js
+++ src/CustomNode.js
@@ -5,17 +5,13 @@ import './CustomNode.css'
 class CustomNode extends Component{
     render () {
         var classNames = require('classnames');
-        const name = this.props.name
-        const nature = this.props.nature
         const highlighted = this.props.highlighted
         const visited = this.props.visited
         const visible = this.props.zoom > this.props.nodeZoom ? true : false
-        let style = {maxHeight : "100%", maxWidth : "100%"}
-        
         if(!visible) return <div className = {classNames({'visible': visible, 'invisible' : !visible && !visited, 'visited': visited})}></div>
         return(
             <div className = {classNames({'visible': visible, 'invisible' : !visible && !visited, 'visited': visited, 'highlighted': highlighted})}>
-                {highlighted ? <Preview resume = {this.props}/> : ""}
+                {highlighted ? <Preview resume = {this.props} style = {{height : '200px', width : '200px'}}/> : ""}
              </div>
          );
   
