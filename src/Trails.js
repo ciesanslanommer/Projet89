d891f8750c80accbaab0640e90c9b8ec1ae38ddf
Natachaliao32 <natachaliao32@gmail.com>
2021-01-20 16:33:42 +0100

    highlightParcours correction + highlight current node in red
    All highlight functions now in trail.js
    + current node is highlighted in red

--- src/Trails.js
+++ src/Trails.js
@@ -1,9 +1,9 @@
 import { Graph } from "react-d3-graph";
 import {React, Component} from 'react';
-import data from './souvenirs.json'
+import data from './souvenirs.json';
 import Background from './assets/fond.png';
 import "./Trails.css";
-import CustomNode from './CustomNode.js'
+import CustomNode from './CustomNode.js';
 
 
 const myConfig = {
@@ -47,6 +47,7 @@ class Trails extends Component {
         //focusedNodeId: "10",
         width: 0, height : 0,
         zoom : 0.5,
+        currentParcours : null,
       };
     };
 
@@ -57,7 +58,7 @@ class Trails extends Component {
     componentWillUnmount () {
       window.removeEventListener('resize', this.measure, false);
     }
-    componentDidMount (){
+    componentDidMount () {
       this.measure();
     }
     measure = e => {
@@ -85,14 +86,13 @@ class Trails extends Component {
 
     }
 
-    zoomChange = (prevZoom, newZoom, e) =>  {
+    zoomChange = (prevZoom, newZoom, e) => {
       // console.log(newZoom);
       this.setState({zoom : newZoom});
       
     }
 
-
-    savePosition = (nodeId, x,y, e)=>{
+    savePosition = (nodeId, x,y, e) => {
       var copy = [...this.state.nodes]
       var item = {...copy[nodeId]};
       item.x = x;
@@ -101,7 +101,7 @@ class Trails extends Component {
       this.setState({nodes : copy});
     }
 
-    customNodeGenerator = (node) =>{
+    customNodeGenerator = (node) => {
       // if(node.highlighted) {
       //   return <Node cx = {node.x} cy = {node.y} fill='green' size = '2000' type = 'square' className = 'node'/>
       // }
@@ -115,14 +115,119 @@ class Trails extends Component {
             />
     }
 
+    componentDidUpdate(prevProps, prevState) {
+      /*** If currentMemory changes  ***/
+      if (prevProps.currentMemory !== this.props.currentMemory) {
+        const currentParcours = this.state.nodes[this.props.currentMemory].parcours;
+
+        /** Handle parcours highlighting **/
+        /* Initialization: clean parcours highlight and set currentParcours to currentMemory's parcours */
+        this.setState({currentParcours: currentParcours});
+        this.removeAllHighlightParcours();
+        /* If the current node is in a parcours, highlight all nodes in the parcours */
+        if (currentParcours != null) {
+          this.highlightParcours(currentParcours);
+        }
+
+        /** Highlights currentNode **/
+        this.removeAllHighlightCurrentNode();
+        console.log(this.props.currentMemory);
+        if(this.props.currentMemory != null) { 
+          this.highlightCurrentNode(this.props.currentMemory);
+        }
+
+      }
+    }
+
     onMouseOverNode = (nodeId, node) => {
-      if(!this.props.docOpen)
-        this.props.onMouseOverNode(node);
-    };
+      /** If document isn't open **/
+      /* currentParcours stays highlighted and parcours mouse overed becomes highlighted */
+      /** If document is open, mouse over shouldn't highlight parcours **/
+      if (!this.props.docOpen) {
+        const parcoursMouseOvered = this.state.nodes[nodeId].parcours;
+        if (parcoursMouseOvered != null) {
+          this.highlightParcours(parcoursMouseOvered.concat(this.state.currentParcours));
+        }
+        else {
+          this.removeAllHighlightParcours();
+        }
+      }
+    }
 
     onMouseOutNode = (nodeId, node) => {
-      // this.props.onMouseOutNode(node);
-    };
+      /** Only currentParcours should stays highlighted **/
+      /* Remove parcours highlight from all nodes and links */
+      this.removeAllHighlightParcours();
+      /* Re-highlight the nodes of currentParcours if currentParcours not null*/
+      if(this.state.currentParcours != null) { 
+        this.highlightParcours(this.state.currentParcours);
+      };
+    }
+
+    /***** HIGHLIGHT FUNCTIONS *****/
+    
+    highlightParcours(parcours) {
+
+      const nodes = this.state.nodes;
+
+      /*** Initialization: set notInParcours state for all ***/
+      nodes.forEach((node) => document.querySelector(`[id="${node.id}"] section`).classList.add("notInParcours"));
+      data.links.forEach((link) => document.querySelector(`[id="${link.source},${link.target}"]`).classList.add("notInParcours"));
+
+      /*** Change to inParcours state only if conditions met ***/
+      parcours.forEach(element => {
+        /** Handle node highlighting **/
+        nodes.forEach((node) => {
+          let htmlNode = document.querySelector(`[id="${node.id}"] section`);
+          if (node.parcours != null && node.parcours.indexOf(element) !== -1) { // if node is in the parcours
+            htmlNode.classList.remove("notInParcours");
+            htmlNode.classList.add("inParcours");
+          }
+        });
+        /** Handle link highlighting **/
+        data.links.forEach((link) => {
+          let htmlLink = document.querySelector(`[id="${link.source},${link.target}"]`);
+          if (nodes[link.source].parcours != null // if source node is in a parcours
+            && nodes[link.target].parcours != null // if target node is in a parcours
+            && nodes[link.source].parcours.indexOf(element) !== -1 // if current node's parcours matches one of source's parcours
+            && nodes[link.target].parcours.indexOf(element) !== -1 // if current node's parcours matches one of target's parcours
+          ) {
+            htmlLink.classList.remove("notInParcours");
+            htmlLink.classList.add("inParcours");
+          }
+        });
+      });
+    }
+
+    removeAllHighlightParcours() {
+      /* Remove node highlighting */
+      data.nodes.forEach( (node) => {
+        let htmlNode = document.querySelector(`[id="${node.id}"] section`);
+        htmlNode.classList.remove("notInParcours"); 
+        htmlNode.classList.remove("inParcours"); 
+      });
+    
+      /* Remove link highlighting */
+      data.links.forEach( (link) => {
+        let htmlLink = document.querySelector(`[id="${link.source},${link.target}"]`);
+        htmlLink.classList.remove("notInParcours"); 
+        htmlLink.classList.remove("inParcours"); 
+      });
+    }
+
+    highlightCurrentNode(nodeId) {
+      let htmlNode = document.querySelector(`[id="${nodeId}"] section`);
+      htmlNode.classList.add('currentNode');
+      console.log('HERE');
+      console.log(htmlNode);
+    }
+
+    removeAllHighlightCurrentNode() {
+      this.state.nodes.forEach( node => {
+        let htmlNode = document.querySelector(`[id="${node.id}"] section`);
+        htmlNode.classList.remove('currentNode');
+      });
+    }
 
 
     // ************************************************************* 
