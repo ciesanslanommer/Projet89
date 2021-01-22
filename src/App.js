c4ba482df212dddf12fcc060e891823a41499fc1
Erwann <erwannlefevre8@gmail.com>
2021-01-22 14:06:29 +0100

    update

--- src/App.js
+++ src/App.js
@@ -143,57 +143,6 @@ class App extends Component {
 
   }
 
-  highlightParcours(currentNode) {
-
-    const nodes = data.nodes;
-
-    /*** Initialization: set notInParcours state for all ***/
-    nodes.forEach( (node) => d3.select(`[id="${node.id}"] section`).attr("class", "notInParcours") );
-    data.links.forEach((link) => d3.select(`[id="${link.source},${link.target}"]`).attr("class", "notInParcours") );
-          
-    /*** Change to inParcours state only if conditions met ***/
-    currentNode.parcours.forEach(element => {
-      /** Handle node highlighting **/
-      nodes.forEach((node) => {
-        let htmlNode = d3.select(`[id="${node.id}"] section`);
-        if(node.parcours!=null && node.parcours.indexOf(element)!==-1) { // if node is in the parcours
-          htmlNode.classed("notInParcours", false);
-          htmlNode.classed("inParcours", true);
-        }
-      });
-      /** Handle link highlighting **/
-      data.links.forEach((link) => {
-        let htmlLink = d3.select(`[id="${link.source},${link.target}"]`);
-        if (nodes[link.source].parcours!=null // if source node is in a parcours
-        && nodes[link.target].parcours!=null // if target node is in a parcours
-        && nodes[link.source].parcours.indexOf(element)!==-1 // if current node's parcours matches one of source's parcours
-        && nodes[link.target].parcours.indexOf(element)!==-1 // if current node's parcours matches one of target's parcours
-        ) { 
-          htmlLink.classed("notInParcours", false);
-          htmlLink.classed("inParcours", true);
-        }
-      });
-    });
-
-  }
-
-  removeHighlightParcours() {
-
-    /* Remove node highlighting */
-    data.nodes.forEach( (node) => {
-      let htmlNode = d3.select(`[id="${node.id}"] section`);
-      htmlNode.classed("notInParcours", null); 
-      htmlNode.classed("inParcours", null); 
-    });
-  
-    /* Remove link highlighting */
-    data.links.forEach( (link) => {
-      let htmlLink = d3.select(`[id="${link.source},${link.target}"]`);
-      htmlLink.classed("notInParcours", null); 
-      htmlLink.classed("inParcours", null); 
-    });
-  };
-
   closeWelcome = e => {
     this.setState({WelcomeOpen: false});
   }
