4bc4455d88dc40feb06ade4e9406be91e35a2e35
Erwann <erwannlefevre8@gmail.com>
2021-01-24 18:45:36 +0100

    route

---
+++ src/Admin.js
@@ -0,0 +1,11 @@
+import { React, Component } from 'react';
+
+class Admin extends Component {
+    render(){
+        return (
+            <div>Coucou</div>
+        )
+    }
+};
+
+export default Admin;
