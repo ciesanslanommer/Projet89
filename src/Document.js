fb6f8b27b1b58947b9eac36d9ec810f42e43ca55
Natachaliao32 <natachaliao32@gmail.com>
2021-01-24 15:24:14 +0100

    disposition done + background + font

--- src/Document.js
+++ src/Document.js
@@ -4,6 +4,7 @@ import {React, PureComponent} from 'react';
 import ReactAudioPlayer from 'react-audio-player';
 import leftArrow from './assets/arrowL.png'
 import rightArrow from './assets/arrowR.png'
+import doc_background from './assets/document_background.jpg';
 
 const Image = (props) => {
     var path = props.parcours ? props.parcours[0] + "/" : ""
@@ -65,15 +66,17 @@ function DocumentButton(props) {
     var type = props.type;
     return (
         <div onClick={props.onClick} className={"button " + type}>
-            {type === "previous" && <img alt="previous" src ={leftArrow}/>}
-            {props.parcours.map( el => 
-                <img
-                    key = {el}
-                    src = {require("./assets/" + el.toLowerCase() +".png" ).default}
-                    alt = {el}
-                />
-            )}
-            {type === "next" && <img alt="previous" src={rightArrow} />}
+            {type === "previous" && <img className = "arrowbutton_img" alt="previous" src ={leftArrow}/>}
+            <div className = "trail_img">
+                {props.parcours.map( el => 
+                    <img
+                        key = {el}
+                        src = {require("./assets/" + el.toLowerCase() +".png" ).default}
+                        alt = {el}
+                    />
+                )}
+            </div>
+            {type === "next" && <img className = "arrowbutton_img" alt="next" src={rightArrow} />}
         </div>
     );
 }
@@ -106,35 +109,45 @@ class Document extends PureComponent {
         let doc = this.displayDoc('main_doc', this.props.nature, this.props.path); // Main document
         let subs = this.props.subs; // Array of secondary documents associated with the main one
         return(
-            <div className="souvenir">
-                <div className="buttons">
-                    {this.state.sources.map(source =>
-                        <DocumentButton 
-                            key={source.id}
-                            onClick={() => this.props.onNextClick(source.id)} 
-                            type="previous" 
-                            parcours= {source.parcours}
-                        />
-                    )}
-                </div>
-                <div className="document">
-                    <h1>{this.props.desc}</h1>
-                    {doc}
-                {subs!=null && 
-                    <div className="sub_docs"> 
-                        {subs.map( (sub) => this.displayDoc(sub.id, sub.nature, sub.path) )}
+            <div className="souvenir" style={{ backgroundImage: "url(" + doc_background + ")" }}>
+                <div id="memory_info">
+                    <div id="date">
+                        <p>25/05/20</p>
                     </div>
-                }
+                    <div id="contributor">
+                        <p>Abraham Lincoln</p>
+                    </div>
+                    <div className="document">
+                        <h1>{this.props.desc}</h1>
+                        {doc}
+                    {subs!=null && 
+                        <div className="sub_docs"> 
+                            {subs.map( (sub) => this.displayDoc(sub.id, sub.nature, sub.path) )}
+                        </div>
+                    }
+                    </div>
                 </div>
                 <div className="buttons">
-                {this.state.targets.map(target =>
-                    <DocumentButton 
-                        key={target.id}
-                        onClick={() => this.props.onNextClick(target.id)} 
-                        type="next" 
-                        parcours={target.parcours}
-                    />
-                )}
+                    <div className="all_previous">
+                        {this.state.sources.map(source =>
+                            <DocumentButton 
+                                key={source.id}
+                                onClick={() => this.props.onNextClick(source.id)} 
+                                type="previous" 
+                                parcours= {source.parcours}
+                            />
+                        )}
+                    </div>
+                    <div className="all_next">
+                        {this.state.targets.map(target =>
+                            <DocumentButton 
+                                key={target.id}
+                                onClick={() => this.props.onNextClick(target.id)} 
+                                type="next" 
+                                parcours={target.parcours}
+                            />
+                        )}
+                    </div>
                 </div>
                 <img 
                     id='cross' 
