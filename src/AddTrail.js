9cd4b81ab3c4871c6871bdf16f1065c89c0c0960
evodable <vodableemma@gmail.com>
2021-01-25 14:30:11 +0100

    Admin form update (create keywords + trails)

---
+++ src/AddTrail.js
@@ -0,0 +1,80 @@
+import { React, Component } from 'react';
+import { ENDPOINT_API } from './constants/endpoints';
+
+class AddTrail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      trail: '',
+      icon_id: '',
+    };
+  }
+
+  getValue = (stateKey, event) => {
+    let value = event.target.value;
+    console.log();
+    console.log(value);
+    this.setState({ [stateKey]: value });
+  };
+
+  postTrail = () => {
+    if (this.state.trail === '') {
+      alert(' le mot clef ne peut pas être vide');
+      return;
+    }
+    fetch(ENDPOINT_API + '/trail', {
+      method: 'POST',
+      body: JSON.stringify({
+        name: this.state.trail,
+        icon_id: this.state.icon_id,
+        pos_x: Math.random() * 1000,
+        pos_y: Math.random() * 1000,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        const value = '';
+        this.setState({
+          keyword: value,
+        });
+        this.props.reloadTrail();
+        alert('mot clef ajouté');
+      });
+  };
+
+  render() {
+    return (
+      <div className='newTrail'>
+        <label>Créer un nouveau parcours</label>
+        <input
+          value={this.state.trail}
+          type='text'
+          placeholder='Ruines'
+          onChange={(e) => this.getValue('trail', e)}
+        />
+        <select
+          name='icons'
+          id='icon_id'
+          onChange={(e) => this.getValue('icon_id', e)}
+        >
+          {this.props.icon.map((icon) => {
+            return (
+              <option key={icon.id} value={icon.id}>
+                {icon.name}
+              </option>
+            );
+          })}
+        </select>
+        <button type='button' onClick={this.postTrail}>
+          Ajouter ce parcours au souvenir
+        </button>
+      </div>
+    );
+  }
+}
+
+export default AddTrail;
--- src/AdminForm.js
+++ src/AdminForm.js
@@ -1,7 +1,9 @@
 import { React, Component } from 'react';
 import './AdminForm.css';
+import AddKeyword from './AddKeyword.js';
 
 import { ENDPOINT_API } from './constants/endpoints';
+import AddTrail from './AddTrail';
 
 class AdminForm extends Component {
   constructor(props) {
@@ -15,9 +17,10 @@ class AdminForm extends Component {
       target_id: '',
       contributeur: '',
       keyword_id: '',
-      keyword: '',
       newTrailName: '',
       subFormat: '',
+      trails: [],
+      trailsLoaded: false,
       icons: [],
       iconLoaded: false,
       submemories: [],
@@ -71,7 +74,12 @@ class AdminForm extends Component {
           // TODO maybe display an error for the user?
         }
       );
+    this.loadTrail();
+    this.loadKeyword();
+  }
 
+  loadKeyword = () => {
+    this.setState({ keywordLoaded: false });
     console.log(`Fetching keyword from ${ENDPOINT_API}/keyword/`);
     fetch(ENDPOINT_API + '/keyword')
       .then((res) => res.json())
@@ -91,29 +99,52 @@ class AdminForm extends Component {
           // TODO maybe display an error for the user?
         }
       );
-  }
+  };
+
+  loadTrail = () => {
+    this.setState({ trailsLoaded: false });
+    console.log(`Fetching keyword from ${ENDPOINT_API}/trail/`);
+    fetch(ENDPOINT_API + '/trail')
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log('Success! trail = ', result);
+          this.setState({
+            trails: result,
+            trailsLoaded: true,
+          });
+        },
+        (error) => {
+          console.error(
+            'Oops, something wrong happened when loading trail',
+            error
+          );
+          // TODO maybe display an error for the user?
+        }
+      );
+  };
 
   getValue = (stateKey, event) => {
-    let value = event.target.value
-    console.log()
-    console.log(value)
-    this.setState({ [stateKey]: value })
-  }
+    let value = event.target.value;
+    console.log();
+    console.log(value);
+    this.setState({ [stateKey]: value });
+  };
 
   closeButtonK = () => {
-    this.setState({ closeButtonK: false })
-  }
+    this.setState({ closeButtonK: false });
+  };
 
   closeButtonT = () => {
-    this.setState({ closeButtonT: false })
-  }
+    this.setState({ closeButtonT: false });
+  };
 
   addElements = (stateKey) => {
-    console.log(stateKey)
-    this.setState({ [stateKey]: true })
-    if(stateKey === "createKeyword") this.closeButtonK()
-    if(stateKey === "createTrail") this.closeButtonT()
-  }
+    console.log(stateKey);
+    this.setState({ [stateKey]: true });
+    if (stateKey === 'createKeyword') this.closeButtonK();
+    if (stateKey === 'createTrail') this.closeButtonT();
+  };
 
   displayDoc = (format) => {
     switch (format) {
@@ -148,22 +179,51 @@ class AdminForm extends Component {
     }
   };
 
-  postRequest(name, description, format, date, icon_id) {
+  postRequest(
+    name,
+    description,
+    format,
+    content,
+    date,
+    icon_id,
+    contributeur,
+    contribution_date,
+    priority,
+    trails, // tableau d'id
+    keywords, // tableau d'id
+    targets, //tableau d'id
+    subs // tableau d'objet de type {name, format, desc, texte ou image ou audio ou yt}
+  ) {
     if (name === '' || description === '' || date === '') {
-      alert('Des champs obligatoires ne sont pas remplis')
+      alert('Des champs obligatoires ne sont pas remplis');
       return;
     }
+
+    let request = {
+      name: name,
+      description: description,
+      format: format,
+      icon_id: icon_id,
+    };
+
+    if (contributeur) request.contributeur = contributeur;
+    if (contribution_date) request.contribution_date = contribution_date;
+    if (priority) request.priority = priority;
+    else request.priority = 1;
+    if (format === 'image') request.image = content;
+    if (format === 'audio') request.audio = content;
+    if (format === 'youtube') request.youtube = content;
+    if (format === 'texte') request.texte = content;
+
+    if (subs) request.subs = subs;
+    if (trails) request.trails = trails;
+    if (targets) request.targets = targets;
+    if (keywords) request.keywords = keywords;
+
     /*~~~~~~~~~~ Post Request ~~~~~~~~~*/
     fetch(ENDPOINT_API + '/memory', {
       method: 'POST',
-      body: JSON.stringify({
-        name: name,
-        description: description,
-        format: format,
-        content: '',
-        date: date,
-        icon_id: icon_id,
-      }),
+      body: JSON.stringify(request),
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
       },
@@ -189,9 +249,8 @@ class AdminForm extends Component {
     let keywords = this.state.keywords;
     return (
       //****************************Formulaire d'ajout de souvenir**************************************** */
-      <div className="mainContainer">
+      <div className='mainContainer'>
         <form className='adminForm'>
-
           {/* *************************************************************** AJOUT DU SOUVENIR *************************************************************** */}
           {/* <MemoryForm 
             onChange={this.getValue}
@@ -206,38 +265,68 @@ class AdminForm extends Component {
             submemories={this.state.submemories}
 
           /> */}
-          <div className="sousForm">
-            <label>Titre du souvenir <abbr> * </abbr></label>
+          <div className='sousForm'>
+            <label>
+              Titre du souvenir <abbr> * </abbr>
+            </label>
             <input
               required
-              className="require"
+              className='require'
               value={this.state.name}
               type='text'
               placeholder='Chute du mur'
-              onChange={(e) => this.getValue("name", e)}
+              onChange={(e) => this.getValue('name', e)}
             />
-            <label>Description <abbr> * </abbr></label>
-            <textarea required className="require" name="description" row="3" cols="33" placeholder='ex : mur de Berlin' maxlength="95" onChange={(e) => this.getValue("description", e)}></textarea>
-            <label>Format du fichier <abbr> * </abbr></label>
-            <select required className="require" name='format' id='format_id' onChange={(e) => this.getValue("format", e)}>
+            <label>
+              Description <abbr> * </abbr>
+            </label>
+            <textarea
+              required
+              className='require'
+              name='description'
+              row='3'
+              cols='33'
+              placeholder='ex : mur de Berlin'
+              maxlength='95'
+              onChange={(e) => this.getValue('description', e)}
+            ></textarea>
+            <label>
+              Format du fichier <abbr> * </abbr>
+            </label>
+            <select
+              required
+              className='require'
+              name='format'
+              id='format_id'
+              onChange={(e) => this.getValue('format', e)}
+            >
               <option value='image'>Image</option>
               <option value='video'>Vidéo</option>
               <option value='youtube'>Lien Youtube</option>
               <option value='texte'>Texte</option>
             </select>
-            {
-              this.displayDoc(this.state.format)
-            }
-            <label>Date de contribution <abbr> * </abbr></label>
-            <input required className="require" type='date' onChange={(e) => this.getValue("date", e)} />
+            {this.displayDoc(this.state.format)}
+            <label>
+              Date de contribution <abbr> * </abbr>
+            </label>
+            <input
+              required
+              className='require'
+              type='date'
+              onChange={(e) => this.getValue('date', e)}
+            />
             <label>Contributeurs (séparer les noms par des virgules) </label>
             <input
               type='text'
               placeholder='Jean Dupont'
-              onChange={(e) => this.getValue("contributeur", e)}
+              onChange={(e) => this.getValue('contributeur', e)}
             />
             <label>Icone de souvenir </label>
-            <select name='icons' id='icon_id' onChange={(e) => this.getValue("icon_id", e)}>
+            <select
+              name='icons'
+              id='icon_id'
+              onChange={(e) => this.getValue('icon_id', e)}
+            >
               {icons.map((icon) => {
                 return (
                   <option key={icon.id} value={icon.id}>
@@ -246,8 +335,14 @@ class AdminForm extends Component {
                 );
               })}
             </select>
-            <label>À quel souvenir est-il relié ? (peut être indépendant) </label>
-            <select name='target' id='target_id' onChange={(e) => this.getValue("target_id", e)}>
+            <label>
+              À quel souvenir est-il relié ? (peut être indépendant){' '}
+            </label>
+            <select
+              name='target'
+              id='target_id'
+              onChange={(e) => this.getValue('target_id', e)}
+            >
               {submemories.map((submemory) => {
                 return (
                   <option key={submemory.id} value={submemory.id}>
@@ -260,46 +355,42 @@ class AdminForm extends Component {
 
           {/* *************************************************************** AJOUT KEYWORDS *************************************************************** */}
 
-          <div className="sousForm keywords" id="KeyWords">
-            <div className="divKeywords">
-              <label>
-                Taguer le souvenir d'un ou plusieurs mots-clés
-          </label>
-              <select name='keywords' id='keyword_id' onChange={(e) => this.getValue("keyword_id", e)}>
+          <div className='sousForm keywords' id='KeyWords'>
+            <div className='divKeywords'>
+              <label>Taguer le souvenir d'un ou plusieurs mots-clés</label>
+              <select
+                name='keywords'
+                id='keyword_id'
+                onChange={(e) => this.getValue('keyword_id', e)}
+              >
                 {keywords.map((keyword) => {
                   return (
                     <option key={keyword.id} value={keyword.id}>
-                      {keyword.name}
+                      {keyword.word}
                     </option>
                   );
                 })}
               </select>
             </div>
-            {
-              this.state.closeButtonK &&
-                <button type="button" onClick={() => this.addElements('createKeyword')} >Ajouter un mot-clé</button>
-            }
-            {
-              this.state.createKeyword &&
-              <div className="divKeywords">
-                <label>Nouveau mot-clé</label>
-                <input
-                  value={this.state.keyword}
-                  type='text'
-                  placeholder='bicentenaire'
-                  onChange={(e) => this.getValue("keyword", e)}
-                />
-                <button type="button">Ajouter ce mot clé à la liste</button>
-              </div>
-            }
+            {this.state.closeButtonK && (
+              <button
+                type='button'
+                onClick={() => this.addElements('createKeyword')}
+              >
+                Ajouter un mot-clé
+              </button>
+            )}
+            {this.state.createKeyword && (
+              <AddKeyword reloadKeyword={this.loadKeyword} />
+            )}
           </div>
           {/* *************************************************************** AJOUT PARCOURS *************************************************************** */}
 
-          <div className="sousForm">
+          <div className='sousForm'>
             <label>
               Choix du parcours (un souvenir peut en avoir 0 ou plusieurs)
-          </label>
-            {this.props.trails.map((trail) => {
+            </label>
+            {this.state.trails.map((trail) => {
               return (
                 <div>
                   <input
@@ -312,45 +403,48 @@ class AdminForm extends Component {
                   <label for={trail.id}>{trail.parcours}</label>
                 </div>
               );
-            })
-            }
-            {
-              this.state.closeButtonT &&
-                <button type="button" onClick={() => this.addElements('createTrail')} >Créer un parcours</button>
-            }
-            {
-              this.state.createTrail &&
-              <div className="newTrail">
-                <label>Créer un nouveau parcours</label>
-                <input
-                  value={this.state.newTrailName}
-                  type='text'
-                  placeholder='Ruines'
-                  onChange={(e) => this.getValue("newTrailName", e)}
-                />
-                <button type="button">Ajouter ce parcours au souvenir</button>
-              </div>
-            }
+            })}
+            {this.state.closeButtonT && (
+              <button
+                type='button'
+                onClick={() => this.addElements('createTrail')}
+              >
+                Créer un parcours
+              </button>
+            )}
+            {this.state.createTrail && (
+              <AddTrail icon={this.state.icons} reloadTrail={this.loadTrail} />
+            )}
           </div>
 
           {/* *************************************************************** AJOUT SUBMEMORY *************************************************************** */}
 
-          <div className="sousForm">
+          <div className='sousForm'>
             <label>Ajouter un document au souvenir</label>
-            <label>Format du fichier <abbr> * </abbr></label>
-            <select required className="require" name='subFormat' id='subFormat_id' onChange={(e) => this.getValue("subFormat", e)}>
+            <label>
+              Format du fichier <abbr> * </abbr>
+            </label>
+            <select
+              required
+              className='require'
+              name='subFormat'
+              id='subFormat_id'
+              onChange={(e) => this.getValue('subFormat', e)}
+            >
               <option value='image'>Image</option>
               <option value='video'>Vidéo</option>
               <option value='youtube'>Lien Youtube</option>
               <option value='texte'>Texte</option>
             </select>
-            {
-              this.displayDoc(this.state.subFormat)
-            }
+            {this.displayDoc(this.state.subFormat)}
           </div>
-
         </form>
-        <button type='button' onClick={() => this.postRequest(name, description, format, date, icon_id)}>
+        <button
+          type='button'
+          onClick={() =>
+            this.postRequest(name, description, format, date, icon_id)
+          }
+        >
           Créer un souvenir
         </button>
       </div>
