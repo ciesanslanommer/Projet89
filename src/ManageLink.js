46fd6093fc5c98859f6d97ca59c72b0823fbc744
evodable <vodableemma@gmail.com>
2021-01-27 19:42:09 +0100

    [clean] Current target in select in ManageLink.js

--- src/ManageLink.js
+++ src/ManageLink.js
@@ -3,82 +3,83 @@ import './AdminForm.css';
 import { ENDPOINT_API } from './constants/endpoints';
 
 class ManageLink extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            trails: this.props.trails,
-            memories: this.props.memories,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      trails: this.props.trails,
+      memories: this.props.memories,
+    };
+  }
 
-      putRequest(trail_id, event) {
-        let value = event.target.value;
-        console.log(value)
+  putRequest(trail_id, event) {
+    let value = event.target.value;
+    console.log(value);
 
-        /*~~~~~~~~~~ Put Request ~~~~~~~~~*/
-        fetch(ENDPOINT_API + '/updatetargetid/' + trail_id, {
-          method: 'PUT',
-          body: JSON.stringify({ target_id :value}),
-          headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-          },
-        })
-          .then((res) => res.json())
-          .then((res) => {
-            console.log(res);
-            // const value = '';
-            // this.setState({
-            //   name: value,
-            //   description: value,
-            //   format: value,
-            //   date: value,
-            // });
-            alert('Le souvenir a bien été en registré !');
-          });
-      }
-    
-    
+    /*~~~~~~~~~~ Put Request ~~~~~~~~~*/
+    fetch(ENDPOINT_API + '/updatetargetid/' + trail_id, {
+      method: 'PUT',
+      body: JSON.stringify({ target_id: value }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        // const value = '';
+        // this.setState({
+        //   name: value,
+        //   description: value,
+        //   format: value,
+        //   date: value,
+        // });
+        alert('Le souvenir a bien été en registré !');
+      });
+  }
 
-    render() {
-        console.log(this.state.trails)
-        const trails = this.state.trails
-        const memories = this.state.memories
-        return (
-            <div className="mainContainer">
-                <h2>Lier un parcours avec un souvenir</h2>
-                <table>
-                    <tr>
-                        <th>Parcours</th>
-                        <th>Souvenirs</th>
-                    </tr>
-                    {trails.map((trail) => {
-                        return (
-                            <tr>
-                                <td key={trail.id}>
-                                    {trail.parcours}
-                                </td>
-                                <td>
-                                    <select
-                                        name='memories'
-                                        onChange={(e) => this.putRequest(trail.id, e)}
-                                    >
-                                        <option value="null">...</option>
-                                        {memories.map((memory) => {
-                                            return (
-                                                <option key={memory.id} value={memory.id}>
-                                                    {memory.name}
-                                                </option>
-                                            );
-                                        })}
-                                    </select>
-                                </td>
-                            </tr>
-                        );
+  render() {
+    console.log(this.state.trails);
+    const trails = this.state.trails;
+    const memories = this.state.memories;
+    return (
+      <div className='mainContainer'>
+        <h2>Lier un parcours avec un souvenir</h2>
+        <table>
+          <tr>
+            <th>Parcours</th>
+            <th>Souvenirs</th>
+          </tr>
+          {trails.map((trail) => {
+            return (
+              <tr>
+                <td key={trail.id}>{trail.parcours}</td>
+                <td>
+                  <select
+                    name='memories'
+                    onChange={(e) => this.putRequest(trail.id, e)}
+                  >
+                    <option value='null'>...</option>
+                    {memories.map((memory) => {
+                      const selected = trail.target_id === memory.id;
+                      return (
+                        <option
+                          key={memory.id}
+                          value={memory.id}
+                          selected={selected}
+                        >
+                          {memory.name}
+                        </option>
+                      );
                     })}
-                </table>
-            </div>
-        );
-    }
+                  </select>
+                </td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
+    );
+  }
 }
 
 export default ManageLink;
--- src/ManageLinkMemory.js
+++ src/ManageLinkMemory.js
@@ -3,89 +3,88 @@ import './AdminForm.css';
 import { ENDPOINT_API } from './constants/endpoints';
 
 class ManageLinkMemory extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            trails: this.props.trails,
-            memories: this.props.memories,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      trails: this.props.trails,
+      memories: this.props.memories,
+    };
+  }
 
-    getValue = (event) => {
-        let value = event.target.value;
-        console.log(value)
-        this.setState({ target_id: value });
-      };
+  getValue = (event) => {
+    let value = event.target.value;
+    console.log(value);
+    this.setState({ target_id: value });
+  };
 
-      postRequest(memory_id, event) {
-        let value = event.target.value;
-        console.log(value)
-        console.log(memory_id)
+  postRequest(memory_id, event) {
+    let value = event.target.value;
+    console.log(value);
+    console.log(memory_id);
 
-        /*~~~~~~~~~~ Put Request ~~~~~~~~~*/
-        fetch(ENDPOINT_API + '/hastrail', {
-          method: 'POST',
-          body: JSON.stringify({ memory_id : Number(memory_id), trail_id : Number(value)}),
-          headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-          },
-        })
-          .then((res) => res.json())
-          .then((res) => {
-            console.log(res);
-            // const value = '';
-            // this.setState({
-            //   name: value,
-            //   description: value,
-            //   format: value,
-            //   date: value,
-            // });
-            alert('Le parcours a bien été en registré !');
-          });
-      }
-    
-    
+    /*~~~~~~~~~~ Put Request ~~~~~~~~~*/
+    fetch(ENDPOINT_API + '/hastrail', {
+      method: 'POST',
+      body: JSON.stringify({
+        memory_id: Number(memory_id),
+        trail_id: Number(value),
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        // const value = '';
+        // this.setState({
+        //   name: value,
+        //   description: value,
+        //   format: value,
+        //   date: value,
+        // });
+        alert('Le parcours a bien été en registré !');
+      });
+  }
 
-    render() {
-        console.log(this.state.trails)
-        const trails = this.state.trails
-        const memories = this.state.memories
-        return (
-            <div className="mainContainer">
-                <h2>Lier un souvenir à un parcours</h2>
-                <table>
-                    <tr>
-                        <th>Souvenirs</th>
-                        <th>Parcours</th>
-                    </tr>
-                    {memories.map((memory) => {
-                        return (
-                            <tr>
-                                <td key={memory.id}>
-                                    {memory.name}
-                                </td>
-                                <td>
-                                    <select
-                                        name='trails'
-                                        onChange={(e) => this.postRequest(memory.id, e)}
-                                    >
-                                        <option value="null">...</option>
-                                        {trails.map((trail) => {
-                                            return (
-                                                <option key={trail.id} value={trail.id}>
-                                                    {trail.parcours}
-                                                </option>
-                                            );
-                                        })}
-                                    </select>
-                                </td>
-                            </tr>
-                        );
+  render() {
+    console.log(this.state.trails);
+    const trails = this.state.trails;
+    const memories = this.state.memories;
+    return (
+      <div className='mainContainer'>
+        <h2>Lier un souvenir à un parcours</h2>
+        <table>
+          <tr>
+            <th>Souvenirs</th>
+            <th>Parcours</th>
+          </tr>
+          {memories.map((memory) => {
+            return (
+              <tr>
+                <td key={memory.id}>{memory.name}</td>
+                <td>
+                  <select
+                    name='trails'
+                    onChange={(e) => this.postRequest(memory.id, e)}
+                  >
+                    <option value='null'>...</option>
+                    {trails.map((trail) => {
+                      return (
+                        <option key={trail.id} value={trail.id}>
+                          {trail.parcours}
+                        </option>
+                      );
                     })}
-                </table>
-            </div>
-        );
-    }
+                  </select>
+                </td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
+    );
+  }
 }
 
 export default ManageLinkMemory;
