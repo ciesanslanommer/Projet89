62831d90e56387490ac912fcd1d7caa0deb616b1
evodable <vodableemma@gmail.com>
2021-02-09 18:43:57 +0100

    Fetch data (new endpoint)

--- .github/workflows/fetchData.js
+++ .github/workflows/fetchData.js
@@ -6,15 +6,15 @@ const fs = require('fs');
 const ENDPOINT_API_DEV = 'http://localhost:3001';
 const ENDPOINT_API_PROD = 'https://projet89-backend.herokuapp.com';
 
-const ENDPOINT_API = process.env.NODE_ENV === 'production'
-    ? ENDPOINT_API_PROD
-    : ENDPOINT_API_DEV;
+const ENDPOINT_API =
+  process.env.NODE_ENV === 'production' ? ENDPOINT_API_PROD : ENDPOINT_API_DEV;
 
 const ENDPOINTS = [
-    ENDPOINT_API + '/node',
-    ENDPOINT_API + '/link',
-    ENDPOINT_API + '/trail',
-    ENDPOINT_API + '/trailbymemory',
+  ENDPOINT_API + '/node',
+  ENDPOINT_API + '/link',
+  ENDPOINT_API + '/trail',
+  ENDPOINT_API + '/trailbymemory',
+  ENDPOINT_API + '/memories',
 ];
 
 // just used for tests
@@ -24,51 +24,51 @@ const ENDPOINTS = [
 //     'https://jsonplaceholder.typicode.com/users'
 // ];
 
-const requestAsync = function(url) {
-    console.log('Fetching', url);
-    return new Promise((resolve, reject) => {
-        request(url, (error, response, body) => {
-            if (error) {
-              console.error(error);
-              process.exit(1);
-              // return reject(err, response, body);
-            }
-            console.log(' - Fetching', url, ': done.');
-            resolve(body);
-            // resolve(JSON.parse(body));
-        });
+const requestAsync = function (url) {
+  console.log('Fetching', url);
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        process.exit(1);
+        // return reject(err, response, body);
+      }
+      console.log(' - Fetching', url, ': done.');
+      resolve(body);
+      // resolve(JSON.parse(body));
     });
+  });
 };
 
 const writeFile = (fileName, data) => {
   console.log('Writing file', fileName);
-  fs.writeFile(fileName, data, error => {
-      if (error) {
-          console.error(error);
-          process.exit(1);
-      }
+  fs.writeFile(fileName, data, (error) => {
+    if (error) {
+      console.error(error);
+      process.exit(1);
+    }
   });
   console.log(' - Writing file', fileName, ': done.');
-} 
+};
 
 const writeFiles = (data) => {
-  const [node, link, trail, trailbymemory] = data;
+  const [node, link, trail, trailbymemory, memories] = data;
   writeFile('../../public/data/node.json', node);
   writeFile('../../public/data/link.json', link);
   writeFile('../../public/data/trail.json', trail);
   writeFile('../../public/data/trailbymemory.json', trailbymemory);
-}
+  writeFile('../../public/data/memories.json', memories);
+};
 
 const fetchData = async () => {
-    console.log('Fetching data');
-    try {
-        const data = await Promise.all(ENDPOINTS.map(requestAsync));
-        writeFiles(data);
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
-
-}
+  console.log('Fetching data');
+  try {
+    const data = await Promise.all(ENDPOINTS.map(requestAsync));
+    writeFiles(data);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
 
 fetchData();
