2bd76a3980c225432bd9ddf8737d5b79bb1fc88e
Aurélien Greffard <aurelien.greffard@gmail.com>
2021-03-20 19:59:06 +0100

    Trying to fix routes

--- src/index.js
+++ src/index.js
@@ -6,20 +6,20 @@ import Admin from './adminComponent/Admin.js';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-console.log("hello")
+console.log("hello", process.env.PUBLIC_URL)
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Switch>
-        <Route exact path="/Projet89">
-          <App preview = {false} />
+        <Route exact path={process.env.PUBLIC_URL + ''}>
+          <App />
         </Route>
-        <Route path="/Projet89/admin">
+        <Route path={process.env.PUBLIC_URL + '/admin'}>
           <Admin />
         </Route>
 
-        <Route path="/Projet89/preview">
-          <App preview = {true} />
+        <Route path={process.env.PUBLIC_URL + '/preview'}>
+          <App preview />
         </Route>
       </Switch>
     </BrowserRouter>
