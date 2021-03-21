5e6fb64f3cb5a404d9f81785dc77dac7f5e3cde5
evodable <vodableemma@gmail.com>
2021-03-21 19:10:28 +0100

    [admin] Update path
    Pull Backend

--- src/adminComponent/LinkForm.js
+++ src/adminComponent/LinkForm.js
@@ -7,7 +7,37 @@ class LinkForm extends Component {
     this.state = {
       icon_id: this.props.update ? this.props.trail.icon_id : this.props.firsticon,
       memory_ids : [],
+      path: [],
+      pathLoaded: false
     };
+  }
+
+  componentDidMount() {
+    console.log(`Fetching trail from ${ENDPOINT_API}/trail/`+this.props.trail.id);
+    fetch(ENDPOINT_API + '/trail/' + this.props.trail.id)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log('Success! trail = ', result);
+          this.setState({
+            path: result.message ? [] : result,
+            // memory_ids : result,
+            pathLoaded: true,
+          });
+          if (result.length){
+            this.setState({
+              memory_ids : result,
+            })
+          }
+        },
+        (error) => {
+          console.error(
+            'Oops, something wrong happened when loading trail',
+            error
+          );
+          // TODO maybe display an error for the user?
+        }
+      );
   }
 
   getValue = (stateKey, event) => {
@@ -29,6 +59,12 @@ class LinkForm extends Component {
     addedMem[Number(e.target.id)] = Number(e.target.value)
     this.setState({memory_ids : addedMem})
   }
+
+  deleteFromMem = (e, indexToDelete) => {
+    console.log(indexToDelete);
+      const newArr = this.state.memory_ids.filter((id, index) => index !== indexToDelete);
+      this.setState({memory_ids : newArr})
+  }
 	
   hasDuplicates = (arr) =>{
       return new Set(arr).size !== arr.length; 
@@ -46,6 +82,7 @@ class LinkForm extends Component {
     fetch(ENDPOINT_API + '/createtrailpath/' + this.props.trail.id, {
       method: 'PUT',
       body: JSON.stringify({
+        previous_ids : this.state.path,
         memory_ids : this.state.memory_ids
       }),
       headers: {
@@ -61,32 +98,36 @@ class LinkForm extends Component {
 
 
   render() {
-    // if used i more than 1 trail the memory doesn't appear in the list
-    const memories = this.props.memories.filter(mem => 
-      (this.props.trailByMemory[mem.id] !== undefined 
-      && this.props.trailByMemory[mem.id].length < 2)
-      || this.props.trailByMemory[mem.id] === undefined
-    )
-
+    // if used it more than 1 trail the memory doesn't appear in the list
+    const memories = this.props.memories
+    // const memories = this.props.memories.filter(mem => 
+    //   (this.props.trailByMemory[mem.id] !== undefined 
+    //   && this.props.trailByMemory[mem.id].length < 2)
+    //   || this.props.trailByMemory[mem.id] === undefined
+    // )
     return (
+    this.state.pathLoaded && 
       <div className='mainContainer'>
         <h2>Lier les couvenirs de  {this.props.trail.parcours}</h2>
         <div className='newTrail mainContainer'>
           {this.state.memory_ids.map((id, index)=> 
-            <select
-            name='memories'
-            id={index}
-            key = {index}
-            onChange={(e) => this.updateMemoryPath(e)}
-            select={id}
-            >
-              <option value={0}>...</option>
-            {memories.map((memory) => 
-                <option key={memory.id} value={memory.id}>
-                 {memory.name}
-                </option>
-            )}
-            </select>
+            <div>
+              <select
+              name='memories'
+              id={index}
+              key = {index}
+              onChange={(e) => this.updateMemoryPath(e)}
+              select={id}
+              >
+                <option value={0}>...</option>
+              {memories.map((memory) => 
+                  <option key={memory.id} value={memory.id} selected={memory.id === id}>
+                  {memory.name}
+                  </option>
+              )}
+              </select>
+              <button onClick={(e) => this.deleteFromMem(e, index)}>x</button>
+            </div>
           )}
          
           <button onClick={!this.state.memory_ids.includes(0) ? this.nextMemory : undefined}>Ajouter un souvenir</button>
